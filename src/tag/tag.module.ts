9295881c33a36aff94e41cd73950b0594c202554
nmhieu2201 <minhhieudz.22012000a@gmail.com>
2023-05-09 22:22:16 +0700

    ss

--- src/tag/tag.module.ts
+++ src/tag/tag.module.ts
@@ -1,5 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TagController } from './tag.controller';
+import { TagService } from './tag.service';
 
-@Module({})
-
-export class TagModule { }
+@Module({
+  controllers: [TagController],
+  providers: [TagService],
+})
+export class TagModule {}
